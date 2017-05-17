7ce3c5152291a2428734b384a221a688ce0d3cd6
Nasthael <Nasthael@user.noreply.github.com>
2017-05-17 17:31:38 +0200

    parametres types preferences

--- app/src/main/java/com/example/anthagonas/wakemehud/Configuration.java
+++ app/src/main/java/com/example/anthagonas/wakemehud/Configuration.java
@@ -1,141 +1,260 @@
 package com.example.anthagonas.wakemehud;
 
-import android.content.ContentResolver;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
+import java.util.List;
 
 /**
- * Created by Vincent on 30/03/17.
+ * A {@link PreferenceActivity} that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ * <p>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the <a
+ * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
+ * API Guide</a> for more information on developing a Settings UI.
  */
+public class Configuration extends AppCompatPreferenceActivity {
+    /**
+     * A preference value change listener that updates the preference's summary
+     * to reflect its new value.
+     */
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
 
-public class Configuration extends AppCompatActivity {
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
 
-                                                //DECLARATIONS
-    //listes pour fragments
-    private ListViewNonScrollable multi_parametres=null;
-    private ListViewNonScrollable simple_parametre_fragment=null;
-    private String[] fragments_coche={"Heure","Notifications", "Météo","Flux RSS", "Calendrier","Aggrégateur"};
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
 
-    //format heure
-    private ListViewNonScrollable simple_parametre_format_heure=null;
-    private String[] format_heure={"Heure","Heure et Date"};
+            } else if (preference instanceof RingtonePreference) {
+                // For ringtone preferences, look up the correct display value
+                // using RingtoneManager.
+                if (TextUtils.isEmpty(stringValue)) {
+                    // Empty values correspond to 'silent' (no ringtone).
+                    //preference.setSummary(R.string.pref_ringtone_silent);
 
-    //verrou
-    private ToggleButton verrouillage=null;
-    private TextView etat_verrou;
+                } else {
+                    Ringtone ringtone = RingtoneManager.getRingtone(
+                            preference.getContext(), Uri.parse(stringValue));
 
-    //bouton sauvegarde des parametres
-    private Button valide=null;
+                    if (ringtone == null) {
+                        // Clear the summary if there was a lookup error.
+                        preference.setSummary(null);
+                    } else {
+                        // Set the summary to reflect the new ringtone display
+                        // name.
+                        String name = ringtone.getTitle(preference.getContext());
+                        preference.setSummary(name);
+                    }
+                }
 
-    //bouton retour au fragment en cours
-    private Button retour=null;
+            } else {
+                // For all other preferences, set the summary to the value's
+                // simple string representation.
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
 
-    //luminosite
-    private SeekBar luminosite;
-    private int brightness; //pour stocker la valeur de luminosite
-    TextView txtPerc;
-    //Content resolver used as a handle to the system's settings
-    private ContentResolver cResolver;
-    //Window object, that will store a reference to the current window
-    private Window window;
+    /**
+     * Helper method to determine if the device has an extra-large screen. For
+     * example, 10" tablets are extra-large.
+     */
+    private static boolean isXLargeTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & android.content.res.Configuration.SCREENLAYOUT_SIZE_MASK) >= android.content.res.Configuration.SCREENLAYOUT_SIZE_XLARGE;
+    }
 
-                                                    //METHODES
+    /**
+     * Binds a preference's summary to its value. More specifically, when the
+     * preference's value is changed, its summary (line of text below the
+     * preference title) is updated to reflect the value. The summary is also
+     * immediately updated upon calling this method. The exact display format is
+     * dependent on the type of preference.
+     *
+     * @see #sBindPreferenceSummaryToValueListener
+     */
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parametres);
-
-        //texte etat_verrou invisible si verrouillage automatique actif
-        etat_verrou=(TextView)findViewById(R.id.etat_verrou);
-        etat_verrou.setVisibility(View.INVISIBLE);
-
-        //attribution des id aux ListViews
-        multi_parametres = (ListViewNonScrollable) findViewById(R.id.choix_fragments);
-        simple_parametre_fragment=(ListViewNonScrollable)findViewById(R.id.unique_liste_fragment);
-        simple_parametre_format_heure=(ListViewNonScrollable)findViewById(R.id.unique_liste_format_heure);
-
-        //SeekBar
-        luminosite=(SeekBar) findViewById(R.id.seekBar);
-        txtPerc = (TextView) findViewById(R.id.txtPercentage);
-
-        //attribution des id aux boutons
-        valide = (Button) findViewById(R.id.valider);
-        retour = (Button) findViewById(R.id.retour);
-        verrouillage = (ToggleButton) findViewById(R.id.verrouillage);
-
-        //gestion des ListView
-        simple_parametre_fragment.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, fragments_coche));
-        simple_parametre_fragment.setItemChecked(0, true);
-
-        simple_parametre_format_heure.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, format_heure));
-        simple_parametre_format_heure.setItemChecked(0, true);
-
-        multi_parametres.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, fragments_coche));
-        //assez sale
-        multi_parametres.setItemChecked(0, true);
-        multi_parametres.setItemChecked(1, true);
-        multi_parametres.setItemChecked(2, true);
-        multi_parametres.setItemChecked(3, true);
-        multi_parametres.setItemChecked(4, true);
-        multi_parametres.setItemChecked(5, true);
-
-
-
-
-
-
-
-
-
-
-        valide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(Configuration.this, "Données modifiées", Toast.LENGTH_LONG).show();
-            }
-        });
-        retour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        verrouillage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changeEtatVerouillage(v);
-            }
-        });
+        setupActionBar();
     }
 
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
-    public void changeEtatVerouillage (View view)
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onIsMultiPane() {
+        return isXLargeTablet(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.pref_headers, target);
+    }
+
+    /**
+     * This method stops fragment injection in malicious applications.
+     * Make sure to deny any unknown fragments here.
+     */
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+                || AffichagePreferenceFragment.class.getName().equals(fragmentName)
+                || RssPreferenceFragment.class.getName().equals(fragmentName)
+                || FragmentsPreferenceFragment.class.getName().equals(fragmentName);
+    }
+
+    /**
+     * This fragment shows general preferences only. It is used when the
+     * activity is showing a two-pane settings UI.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class AffichagePreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_affichage);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+
+            bindPreferenceSummaryToValue(findPreference("example_list"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), Configuration.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * This fragment shows notification preferences only. It is used when the
+     * activity is showing a two-pane settings UI.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class RssPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_rss);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("example_text_entrer_rss"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), Configuration.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * This fragment shows data and sync preferences only. It is used when the
+     * activity is showing a two-pane settings UI.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class FragmentsPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_gestion_fragments);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("liste_fragments_choix_affichage_defaut"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), Configuration.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onBackPressed()
     {
-        //si le togglebutton est en mode "on", ca renvoie "true", sinon, ca revoie "false"
-        boolean check=((ToggleButton)view).isChecked();
-        //si le toggle est en mode "on"
-        if(check)
-        {
-            etat_verrou.setVisibility(View.VISIBLE);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        }
-        else
-        {
-            etat_verrou.setVisibility(View.INVISIBLE);
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        }
-
+        Intent myIntent = new Intent(Configuration.this, WakeMeHUD.class); // Chargement de l'activite parametres
+        finish();
+        startActivity(myIntent); // lancement de l'activite
     }
-
 }
